e93ef68c248bf3ee3a76a98ae96c4a38aaae18c4
Ešvovičius Valentinas <valentinas.esvovicius.2@mif.stud.vu.lt>
2019-10-13 12:51:52 +0300

    Add basic moves

---
+++ Assets/Scripts/Boy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("right"))
+        {
+            transform.position += new Vector3(1, 0, 0);
+        }
+        if (Input.GetKeyDown("left"))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+        }
+    }
+}
