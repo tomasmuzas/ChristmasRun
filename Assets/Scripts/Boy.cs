daa87fdc060367bc0d241d4d5692d11ebc9c1928
Ešvovičius Valentinas <valentinas.esvovicius.2@mif.stud.vu.lt>
2019-10-13 13:10:21 +0300

    Add boy jump

--- Assets/Scripts/Boy.cs
+++ Assets/Scripts/Boy.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Boy : MonoBehaviour
 {
+    private Rigidbody rigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -20,6 +22,10 @@ public class Boy : MonoBehaviour
         if (Input.GetKeyDown("left"))
         {
             transform.position += new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKeyDown("up"))
+        {
+            rigidbody.velocity = new Vector3(0, 4, 0);
         }
     }
 }
