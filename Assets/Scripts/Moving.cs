701684752485bab9056be645ef8e5fde65e96e5b
MariusKurbakovas <marius.kurbakovas@gmail.com>
2019-10-13 14:48:16 +0300

    add moving script

---
+++ Assets/Scripts/Moving.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour
+{
+    private Rigidbody _body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _body.velocity = new Vector3(0, 0, -GameManager.GameSpeed * 100);
+    }
+}
