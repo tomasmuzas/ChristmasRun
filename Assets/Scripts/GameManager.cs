ee2561b2a9fd45ec8a1d2c63513ce3ef4b535b45
Tomas Mūžas <muzas.tomas@gmail.com>
2019-10-13 22:07:11 +0300

    Tidy up the code

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,7 +6,7 @@ public class GameManager : MonoBehaviour
     public float InitialGameSpeed;
     public float SpawnSpeed;
     public static float GameSpeed;
-    public GameObject MyPrefab;
+    public Spawnable SpawnablePrefab;
     public static Random Rnd = new Random();
 
     // Start is called before the first frame update
@@ -24,11 +24,10 @@ public class GameManager : MonoBehaviour
 
     void SpawnObject()
     {
-        var spawnable = MyPrefab.GetComponent<Spawnable>();
-        if (spawnable != null && spawnable.SpawnPositions != null && spawnable.SpawnPositions.Count > 0)
+        if (SpawnablePrefab?.SpawnPositions?.Count > 0)
         {
-            var location = spawnable.SpawnPositions[Rnd.Next(0, spawnable.SpawnPositions.Count)];
-            Instantiate(MyPrefab, location.Position, Quaternion.identity);
+            var location = SpawnablePrefab.SpawnPositions[Rnd.Next(0, SpawnablePrefab.SpawnPositions.Count)];
+            Instantiate(SpawnablePrefab, location.Position, Quaternion.identity);
         }
     }
 }
