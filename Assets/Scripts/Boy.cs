682addf6edec6d366a297d2a153d31fc2644aa62
Ešvovičius Valentinas <valentinas.esvovicius.2@mif.stud.vu.lt>
2019-10-13 13:38:19 +0300

    Jump mechanics

--- Assets/Scripts/Boy.cs
+++ Assets/Scripts/Boy.cs
@@ -1,29 +1,37 @@
-﻿using System.Collections;
+﻿using Assets.Scripts;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Boy : MonoBehaviour
 {
     private Rigidbody rigidbody;
+    private Lane lane;
+    private bool _jumping;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        lane = Lane.Middle;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("right"))
+        _jumping = rigidbody.velocity != Vector3.zero;
+        if (Input.GetKeyDown("right") && lane != Lane.Right && !_jumping)
         {
             transform.position += new Vector3(1, 0, 0);
+            lane++;
         }
-        if (Input.GetKeyDown("left"))
+        if (Input.GetKeyDown("left") && lane != Lane.Left && !_jumping) 
         {
             transform.position += new Vector3(-1, 0, 0);
+            lane--;
         }
-        if (Input.GetKeyDown("up"))
+        if (Input.GetKeyDown("up") && !_jumping)
         {
             rigidbody.velocity = new Vector3(0, 4, 0);
         }
