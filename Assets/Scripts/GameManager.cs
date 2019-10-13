5f15839c9b1edd836f52360e90c95e8859329543
MariusKurbakovas <marius.kurbakovas@gmail.com>
2019-10-13 16:45:16 +0300

    random spawning christmas trees

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
+using Random = System.Random;
 
 public class GameManager : MonoBehaviour
 {
     public float InitialGameSpeed;
     public float SpawnSpeed;
     public static float GameSpeed;
-    public GameObject myPrefab;
+    public GameObject MyPrefab;
+    public static Random Rnd = new Random();
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +24,11 @@ public class GameManager : MonoBehaviour
 
     void SpawnObject()
     {
-        Instantiate(myPrefab, new Vector3(0, 0, 2.631f), Quaternion.identity);
+        var spawnable = MyPrefab.GetComponent<Spawnable>();
+        if (spawnable != null && spawnable.SpawnPositions != null && spawnable.SpawnPositions.Count > 0)
+        {
+            var location = spawnable.SpawnPositions[Rnd.Next(0, spawnable.SpawnPositions.Count)];
+            Instantiate(MyPrefab, location.Position, Quaternion.identity);
+        }
     }
 }
