ade7a4dce050831fb223c6f74e6d8a9a8642b49b
Ešvovičius Valentinas <valentinas.esvovicius.2@mif.stud.vu.lt>
2019-10-17 22:15:19 +0300

    Add game score

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using Random = System.Random;
 
 public class GameManager : MonoBehaviour
@@ -8,18 +9,24 @@ public class GameManager : MonoBehaviour
     public static float GameSpeed;
     public Spawnable SpawnablePrefab;
     public static Random Rnd = new Random();
+    public Text PointsText;
+    private int Points;
+    public float PointsSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
         GameSpeed = InitialGameSpeed;
         InvokeRepeating("SpawnObject", SpawnSpeed, SpawnSpeed);
+        Points = 0;
+        SetPointsText();
+        InvokeRepeating("AddPoints", 0f, PointsSpeed);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        SetPointsText();
     }
 
     void SpawnObject()
@@ -29,5 +36,15 @@ public class GameManager : MonoBehaviour
             var location = SpawnablePrefab.SpawnPositions[Rnd.Next(0, SpawnablePrefab.SpawnPositions.Count)];
             Instantiate(SpawnablePrefab, location.Position, Quaternion.identity);
         }
+    }
+
+    void SetPointsText()
+    {
+        PointsText.text = $"Score: {Points}";
+    }
+
+    void AddPoints()
+    {
+        Points++;
     }
 }
