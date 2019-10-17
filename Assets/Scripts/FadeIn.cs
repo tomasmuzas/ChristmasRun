c47ebb575a84614a32a5b8db9c6b893f6a3e10b6
MariusKurbakovas <marius.kurbakovas@gmail.com>
2019-10-17 17:15:03 +0300

    christmas tree fade in

---
+++ Assets/Scripts/FadeIn.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FadeIn : MonoBehaviour
+{
+    private Renderer _renderer;
+    private Material _material;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _renderer = GetComponent<Renderer>();
+        _material = _renderer.material;
+        StartCoroutine(FadeControl());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    IEnumerator FadeControl()
+    {
+        var fadeAmount = 0f;
+        var color = _material.color;
+
+        color.a = fadeAmount;
+        _renderer.material.color = color;
+
+        for (float f = 0.05f; f <= 1; f += 0.05f)
+        {
+            color.a = f;
+            _renderer.material.color = color;
+            yield return new WaitForSeconds(0.005f);
+        }
+    }
+}
