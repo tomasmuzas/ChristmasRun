dd8c2a3330617097b8fa97e6009c602cb390fb60
MariusKurbakovas <marius.kurbakovas@gmail.com>
2019-10-24 11:10:13 +0300

    create rotation script, make the gift rotate

---
+++ Assets/Scripts/Behaviour/Rotating.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Rotating : MonoBehaviour
+{
+    public float RotationSpeed;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.Rotate(Vector3.up, RotationSpeed * Time.deltaTime);
+    }
+}
