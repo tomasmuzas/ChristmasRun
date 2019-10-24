1d84519f415f5f07742d35ecd64f43fdc84d4962
Tomas Mūžas <muzas.tomas@gmail.com>
2019-10-24 19:06:45 +0300

    Global style adjustments

--- Assets/Scripts/Ground.cs
+++ Assets/Scripts/Ground.cs
@@ -2,7 +2,8 @@
 
 public class Ground : MonoBehaviour
 {
-    public Material material;
+    private Material material;
+    public float SpeedMultiplier = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +16,7 @@ public class Ground : MonoBehaviour
     {
         if (Time.timeScale > 0)
         {
-            material.mainTextureOffset += new Vector2(0, -GameManager.GameSpeed);
+            material.mainTextureOffset += new Vector2(0, -GameManager.GameSpeed * SpeedMultiplier);
         }
     }
 }
